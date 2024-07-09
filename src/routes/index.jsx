bb40976efb096b4c30049a6f02cd7138c2b2d4a7
Thiago Caetano <caetanosbr@gmail.com>
2024-07-08 23:48:39 -0300

    update

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter  } from "react-router-dom";
 
-import { useAuth } from "../hooks/auth" 
+import { useAuth } from "../hooks/auth";
 
 import { AppRoutes } from "./app.routes";
 import { AuthRouts } from "./auth.routes"
