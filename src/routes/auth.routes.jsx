e40dd7a7077e1f07e54bb41864ef0ae6be1272e9
Thiago Caetano <caetanosbr@gmail.com>
2024-07-08 23:56:42 -0300

    update2

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -1,15 +1,13 @@
-//Aqui eu importaria as bibliotecas
+
 import { Routes, Route } from "react-router-dom";
 
-//Aqui eu importarei minhas páginas
-import { Signin } from "../pages/Signin";
+import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
 
-//Aqui eu estou fazendo a exportação da função
+
 export function AuthRouts(){
     return(
-        //O componente Routes envolverá todas as minhas rotas
-        //O componente Route receberá a página em questão e indicará qual endereço de URL referencia cada página
+
         <Routes>
             <Route path="/" element={<SignIn />}/>
             <Route path="/register" element={<SignUp />}/>
